36c2f28c9a9b3eb2b01e07e82b84fd718bdb06a1
Moha <mzw@olymtech.com>
2020-08-04 09:56:32 +0800

    Json

--- src/main/java/com/example/demo/scheduled/TestScheduled.java
+++ src/main/java/com/example/demo/scheduled/TestScheduled.java
@@ -2,10 +2,15 @@ package com.example.demo.scheduled;
 
 import com.example.demo.util.HttpUtil;
 import com.example.demo.util.PropertiesUtil;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.elasticsearch.common.Strings;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,6 +27,9 @@ public class TestScheduled {
     public static String SJZ_TOKEN = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=6258d09a-6244-4858-95e0-54d738b2b2d7";
 
     private static String soupUrl = "https://api.qinor.cn/soup/";
+
+    private static String soupUrl1 = "http://api.tianapi.com/txapi/dujitang/index?key=aad834783cbc454c77b995a38476d428";
+
     public static String[] NAMES = {"沈健力","毛泽威","黄秀娟","程月娇","何胜东","赵万梓","刘婷","潘维健"};
     private static String[] MOBILES = {"15757116539","15728007839","18858090722","15067480436","18815287551","15258316988","17636218668","15867461151"};
 //    private int dayIndex = 3;
@@ -82,22 +90,34 @@ public class TestScheduled {
     @Scheduled(cron = "0 15 9 ? * MON-FRI")
     public void everyDay() {
         String content = ack1();
-
         System.out.println(HttpUtil.doPost(SJZ_TOKEN,content));
     }
 
     public String ack1(){
-        String soup =  HttpUtil.doGet(soupUrl,null,null,null,false);
-        String text = String.format("早上好！快乐时间：%s",soup);
+        String jt = getJtStr();
+        String text = String.format("早上好！鸡汤时间：%s",jt);
         String content = "{\n" +
                 "    \"msgtype\": \"text\",\n" +
                 "    \"text\": {\n" +
-                "        \"content\": \"" + text +
+                "        \"content\": \"" + text +"\""+
                 "    }\n" +
                 "}";
 
         System.out.println(content);
         return content;
+    }
+
+    private String getJtStr(){
+        String soup =  HttpUtil.doGet(soupUrl1,null,null,null,false);
+        if(Strings.isNullOrEmpty(soup)){
+            soup = HttpUtil.doGet(soupUrl,null,null,null,false);
+            return soup;
+        }
+        Map map =  new Gson().fromJson(soup,Map.class);
+        List<Map> mapList = (List<Map>) map.get("newslist");
+        Map content = mapList.get(mapList.size()-1);
+        String jt = (String) content.get("content");
+        return jt;
     }
 
 }
